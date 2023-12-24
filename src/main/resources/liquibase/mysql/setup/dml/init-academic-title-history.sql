780a1ccda7ea133343bd9482739cd46dc86f96e1
milan-stankovic-dev <milance.za019@gmail.com>
2023-12-24 10:50:41 +0100

    Finished liquibase scripts for dml

---
+++ src/main/resources/liquibase/mysql/setup/dml/init-academic-title-history.sql
@@ -0,0 +1,17 @@
+insert into tbl_academic_title_history (start_date, end_date,
+                                        academic_title_id,
+                                        scientific_field_id,
+                                        member_id)
+values ("2021-09-05", "2022-09-05",
+        (select id from tbl_academic_title where name = "Emeritus"),
+        (select id from tbl_scientific_field where field_name = "HCI"),
+        (select id from tbl_member where first_name = "Pera" and last_name = "Peric"));
+insert into tbl_academic_title_history (start_date, end_date,
+                                        academic_title_id,
+                                        scientific_field_id,
+                                        member_id)
+VALUES ("2020-11-20",
+        "2021-11-20",
+        (select id from tbl_academic_title where name = "Lecturer"),
+        (select id from tbl_scientific_field where field_name = "Digital marketing"),
+        (select id from tbl_member where first_name = "Sima" and last_name = "Simic"));
